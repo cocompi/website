55b052abc4046ce3c788fc32c6a7a8811d77d5eb
cocompi <83931729+cocompi@users.noreply.github.com>
2026-04-05 01:27:53 +0100

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -10,7 +10,7 @@ document.querySelectorAll(".gallery").forEach(gallery => {
     // 🔴 CRITICAL FIX: clamp height HARD
     const targetRowHeight = Math.min(
       window.innerHeight * rowHeightFactor,
-      160   // ← THIS fixes your issue
+      260   // ← THIS fixes your issue
     );
 
     let newHTML = "";
